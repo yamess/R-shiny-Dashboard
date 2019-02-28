e12569cc8e06fd736ba223fbe24151eecb9248e6
boukari yameogo <yameogo.boukari@gmail.com>
2019-02-28 17:40:06 -0500

    Modification-Ajout filtres
    Mise à jour de toute l'application. Cette version fonctionne correctement sur le serveur shinyapps.io. Les filtres de l'application sont incluses

--- server.R
+++ server.R
@@ -1,52 +1,189 @@
-# appel du fichier global.r pour recuperer les variables dans ce fichier
+
+#-------------------------------------------server.R -----------------------------------------#
+# Ce fichier contient lest le coeur de l'application. il est le backend de l'application. Il  #
+# reçoit les requetes de l'interface, les traitent, et les renvoie à l'interface pour les     #
+# afficher.Les variables de cet fichier sont locales et non globales.                         #
+#---------------------------------------------------------------------------------------------#
+
+
+#--------------------- Appel du contenu ou des variables du fichier global.R -----------------#
 source("./global.r", local = TRUE)
 
-# Define server logic required to draw a histogram
+#--------------------------- Debut de des instruction server (backend) -----------------------#
 shinyServer(function(input, output) {
   
+#-------------------- Raccoucircement de certaines chaine de caractères pour faciliter l'affichage ----------------
+  Type_Cara$CARACT_NOM_FR[which(Type_Cara$CARACT_NOM_FR == "Services automobile et stationnement")] <- rep("Auto_Parking")
+  Type_Cara$CARACT_NOM_FR[which(Type_Cara$CARACT_NOM_FR == "Modes de paiement")] <- rep("Paiement")
+  Adr_Cara$CARACT_NOM_FR[which(Adr_Cara$CARACT_NOM_FR == "Services automobile et stationnement")] <- rep("Auto_Parking")
+  Adr_Cara$CARACT_NOM_FR[which(Adr_Cara$CARACT_NOM_FR == "Modes de paiement")] <- rep("Paiement")
+  
+#---------------------KPI: nombre total d'établissement de la province --------------
   output$total_hotel_box <- renderValueBox({
-    #nbr total de residences
-    nbr_hotel <- length((unique(etablissements$ETBL_ID)))  # calcul du nombre d'établissement au total
-    valueBox(h3(nbr_hotel),"Total Hotel",icon = icon("home"),color = "aqua") # injecter le nombre d'etablissement dans infobox
+    nbr_hbrg <- length((unique(ETBL_Adr$ETBL_ID)))  # calcul du nombre d'établissement au total
+    valueBox(h3(nbr_hbrg),"Total Établissement",icon = icon("home"),color = "aqua") # injecter le nombre d'etablissement dans infobox
   })
   
+#---------------------KPI: nombre total de municipalité ------------------------------
   output$total_City_box <- renderValueBox({
-    #nbr total localité
-    nbr_ville <- length(unique(as.character(territoires$TERR_ZONE_NOM_EN)))
-    valueBox(h3(nbr_ville),"Total City", icon = icon("map"), color = "blue")
+    nbr_muni <- length(unique(ETBL_Adr$ADR_MUNICIPALITE))
+    valueBox(h3(nbr_muni),"Total Municipalité", icon = icon("map"), color = "blue")
   })
   
-  #Calcul de la variable du nombre total de type qui est Total Type
+#---------------------KPI: nombre total de type d'établissements----------------------
   output$total_type_box <- renderValueBox({
-    #brt total de type de residence
-    type_etbl$ETBL_TYPE_EN <- as.character(type_etbl$ETBL_TYPE_EN)
-    list_type_etbl <- unique(type_etbl$ETBL_TYPE_EN[which(type_etbl$ETBL_TYPE_EN != "")])
-    valueBox(h3(length(list_type_etbl)),"Total Type", icon = icon("th"), color = "aqua")
+    nbr_type_hbrg <- length(unique(Type_Cara$ETBL_TYPE_FR))
+    valueBox(h3(nbr_type_hbrg),"Total Type", icon = icon("th"), color = "aqua")
+  })
+
+  #------------------------ Barchart des type d'établissement ------------------------
+  #Création du graphique
+  output$bar_chart_type <- renderPlot({
+    
+    if (input$choix_ville == "Tous") {
+      tampon <- data.frame(table(unique(Type_Cara[,c(2,3)])$ETBL_TYPE_FR)) #On determine les frequences des differents types
+    } else {
+      tmp <- unique(Adr_Cara[which(Adr_Cara$ADR_MUNICIPALITE == input$choix_ville),2])
+      tampon <- unique(Type_Cara[which(Type_Cara$ETBL_ID %in% tmp),c(2,3)])$ETBL_TYPE_FR
+      tampon <- data.frame(table(tampon))
+    }
+
+    names(tampon) <- c("Type","Frequence") # Changement des noms des colonnes
+    tampon$Type <- as.character(tampon$Type) #Convertir en class caractère afin de remplcer les caractères longs
+    tampon$Type[which(tampon$Type == "Chalet / appartement / résidence de tourisme")] <- rep("Chalet/App/Residence")#Raccourcir la chaine
+    tamp_graph_static <- tampon
+    #rm(tampon)
+    graph <- ggplot(tamp_graph_static, aes(x = Type, y = Frequence)) +
+      geom_bar(stat = "identity", aes(fill = Type)) +
+      geom_text(aes(label = Frequence), vjust = 1, size = 4.0) +
+      theme(axis.text.x = element_blank()) +
+      
+      #Condition sur l'affichage du titre du graphique
+      if (input$choix_ville == "Tous") {
+        ggtitle(paste("Repartition par type d'établissement au Québec"))      
+      } else {ggtitle(paste("Repartition par type d'établissement à",input$choix_ville))}
+    
+    graph
   })
   
-  output$bar_chart <- renderPlotly({
-    type_etbl$ETBL_TYPE_EN <- as.character(type_etbl$ETBL_TYPE_EN)
-    t <- data.frame(table(type_etbl[which(type_etbl$ETBL_TYPE_EN !=""),5]))
-    colnames(t) <- c("Type","Total")
-    t <- arrange(t, desc(t$Total))
-    p <- ggplot(data = t, aes(x=Type, y=Total, fill=Type)) +
-      geom_bar(stat = "identity") +
-      geom_text(aes(label=Total), vjust=-0.3, size = 3.0) +
-      theme(axis.text.x = element_blank())
-    p <- ggplotly(p)
-    p
+#---------------------- Barchart des caractéristiques ---------------------------
+  output$bar_chart <- renderPlot({
+  
+    if (input$choix_ville == "Tous") {
+      tmp_interactif <- data.frame(table(Type_Cara[which(Type_Cara$CARACT_NOM_FR == input$filtre),6]))
+      names(tmp_interactif) <- c("Caracteristique","Frequence") #Changement des noms de colonnes
+      graph <- ggplot(tmp_interactif, aes(x = Caracteristique, y = Frequence)) +
+        geom_bar(stat = "identity", aes(fill = Caracteristique)) +
+        geom_text(aes(label = Frequence), vjust = 1, size = 3.0) +
+        theme(axis.text.x = element_blank()) +
+        ggtitle(paste("Repartition des établissements par",input$filtre))
+      
+      graph #on affiche le graphique
+      
+    } else {
+      
+      tmp_interactif <- Adr_Cara[which(Adr_Cara$ADR_MUNICIPALITE == input$choix_ville),c(4,5)]
+      tmp_interactif <- data.frame(table(tmp_interactif[which(tmp_interactif$CARACT_NOM_FR == input$filtre),2]))
+      names(tmp_interactif) <- c("Caracteristique","Frequence")
+      graph <- ggplot(tmp_interactif, aes(x = Caracteristique, y = Frequence)) +
+        geom_bar(stat = "identity", aes(fill = Caracteristique)) +
+        geom_text(aes(label = Frequence), vjust = 1, size = 3.0) +
+        theme(axis.text.x = element_blank()) +
+        ggtitle(paste("Repartition par",input$filtre,"pour la ville de",input$choix_ville))
+      
+      graph #On affiche le graphique
+    }
   })
   
+#---------------------- Tableau des liste des Établissements----------------------------  
+  #Tire du tableau de lliste des établissements
+  output$titre_tab <- renderText({
+    if (input$choix_ville == "Tous") {
+      titre <- "Liste des établissements au Québec"
+      titre
+    } else {
+      titre <- paste("Liste des établissements à",input$choix_ville)
+      titre
+    }
+  })
+  #Visualisation du tableau des établissements
+  output$list_hbgr <- DT::renderDT({
+    if (input$choix_ville == "Tous") { #liste des établissements de toutes les municipalités
+      tp <- ETBL_Adr[,c(3,4)]
+      tp
+    } else { # On affiche la liste des établissement de la manucipalité choisie
+      tp <- ETBL_Adr[which(ETBL_Adr$ADR_MUNICIPALITE == input$choix_ville),c(3,4)]
+      tp
+    }
+  }, options = list(pageLength = 5))
+  
+  
+#----------------------- Map de la repartition des établissements -------------------------
+  #titre du mao
+  output$titre_map <- renderText({
+    if (input$choix_ville == "Tous") {
+      titre <- "Regroupement des établissements au Québec"
+      titre
+    } else {
+      titre <- paste("Map des établissement à",input$choix_ville)
+      titre
+    }
+  })
+  
+  # CONSTRUCTION DE LA MAP
   output$map <- renderLeaflet({
-    cart_graph <- leaflet()
-    cart_graph <- addTiles(cart_graph)
-    #cart_graph <- addMarkers(cart_graph, lat = coord$Latitude,lng = coord$Longitude, label = as.character(coord$Adresse_Civique))
-    cart_graph <- addCircleMarkers(cart_graph, lat = coord$Latitude,lng = coord$Longitude,radius = 0.05, color = "#368BCD",
-                                   clusterOptions = markerClusterOptions(),label = as.character(coord$Adresse_Civique))
-    cart_graph <- setView(cart_graph, 
-                          lat = mean(adresses$ADR_LATITUDE, na.rm = TRUE), 
-                          lng = mean(adresses$ADR_LONGITUDE, na.rm = TRUE), zoom = 5)
-    cart_graph 
+    
+    #Filtre des positions à afficher selon la municipalité
+    if (input$choix_ville == "Tous") { # Lorsque le choix est pour toutes les municipalités
+      # Debut de la construction de la map
+      map_graph <- leaflet()
+      map_graph <- addTiles(map_graph)
+      map_graph <- addCircleMarkers(map_graph, lat = ETBL_Adr$ADR_LATITUDE,lng = ETBL_Adr$ADR_LONGITUDE, radius = 4, color = "#0073B7",
+                                    clusterOptions = markerClusterOptions(),label = as.character(ETBL_Adr$ETBL_NOM_FR))
+      map_graph <- setView(map_graph, 
+                           lat = mean(ETBL_Adr$ADR_LATITUDE, na.rm = TRUE), 
+                           lng = mean(ETBL_Adr$ADR_LONGITUDE, na.rm = TRUE), zoom = 7)
+      #affichage de la map
+      map_graph 
+      
+    } else {#Lorsque le choix est porté sur une municipalité spécifique
+      
+      # Debut de la construction de la map
+      map_graph <- leaflet()
+      map_graph <- addTiles(map_graph)
+      
+      #Obtenit Lat et Long selon la municipalité selectionnée comme filtre
+      tampon_choix <- ETBL_Adr[which(ETBL_Adr$ADR_MUNICIPALITE == input$choix_ville),c(2,3,5,7,8)]
+      
+      #map_graph <- addCircleMarkers(map_graph, lat = tampon_choix$ADR_LATITUDE,lng = tampon_choix$ADR_LONGITUDE, radius = 4, color = "#0073B7",
+                                    #clusterOptions = markerClusterOptions(),label = as.character(tampon_choix$ETBL_NOM_FR))
+      
+      map_graph <- addMarkers(map_graph,lat = tampon_choix$ADR_LATITUDE,lng = tampon_choix$ADR_LONGITUDE, label = as.character(tampon_choix$ETBL_NOM_FR))
+      map_graph <- setView(map_graph, 
+                           lat = mean(tampon_choix$ADR_LATITUDE, na.rm = TRUE), 
+                           lng = mean(tampon_choix$ADR_LONGITUDE, na.rm = TRUE), zoom = 10)
+      #affichage de la map
+      map_graph
+    }
+    
   })
-  
+
+#------------------------Visualisation des tableaux dans la 2ieme tab "Data" ----------------
+  output$data_viz_out <- DT::renderDT({
+    #recuperation de la valeur selectionner en input
+    tab <- input$data_viz_in
+    
+    # Condition sur l'affichage selon la tab selectionné
+    if (tab == "JOINTURE ETBL & ADRESSES") {
+      tab_out <- ETBL_Adr[,c(-1)]
+      colnames(tab_out) <- c("ID","NOM","ADRESS CIVIQUE","MUNICIPALITÉ","CODE POSTAL","LATITUDE","LONGITUDE") #Renommer les colonnes
+    } else {
+        tab_out <- Type_Cara[,c(-1)]
+    }
+    #affichage de la tab selectionnée
+    tab_out
+  }, filter = "top")
 })
+
+
+#-------------------------------------- THE END -------------------------------------------
--- ui.R
+++ ui.R
@@ -1,54 +1,229 @@
-# Define UI for application that draws a histogram
 
-# Entete de l'application
+#------------------------------------------- UI.R --------------------------------------------#
+# Ce fichier contient les intructions pour la création de l'interface graphique. Il ne traite #
+# que de l'affichage des composants. Variables définis ici sont envoyés à la partie server.R  #
+# pour traitement des requetes.                                                               #
+#---------------------------------------------------------------------------------------------#
+
+#-------------------------- Debut de l'interface graphique ------------------------------------
+
+#-------------------------- Création de l'entete de l'application -----------------------------
 header <- dashboardHeader(title = "Hebergement Quebec")
 
-# Barre lateral de l'application
+
+#-------------------------- Création de la Barre lateral de l'application ---------------------
 sidebar <- dashboardSidebar(
-  titlePanel("HEC Montréal"),
   sidebarMenu(
     menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
     menuItem("Data", tabName = "data_table", icon = icon("th")),
     menuItem("Team", tabName = "team", icon = icon("user"))
-  )
+  ), collapsed = TRUE #Permet de cacher a barre laterale à l'ouverture de l'application
 )
 
-# Corps de l'application
+#-------------------------- Création du corps de l'application --------------------------------
 body <- dashboardBody(
   fluidPage(
     tabItems(
+      
+      #----------------------------- Section 1: le dashboard ---------------------------------
       tabItem(tabName = "dashboard",
+              
+              # ---------------------- Ligne 1: Affichage des box pour les kpi ----------------
               fluidRow(
-                valueBoxOutput("total_hotel_box"), # sorie de la boite a info pour totatl accomodation
+                valueBoxOutput("total_hotel_box"),
                 valueBoxOutput("total_City_box"),
                 valueBoxOutput("total_type_box")
-                #gradientBox(title = "Filter", status = "success", solidHeader = TRUE,
-                #selectInput(inputId = "filtre2", label = "Choose:", choices = c("All","Type")))
               ),
+              
+              # ---------------------- Ligne 2: Filtres et les bars chart ---------------------
               fluidRow(
-                box(width = 8,height = "300px", plotlyOutput("bar_chart",height = "280px"),status = "primary"),
-                box(title = "Filtre", width = 4, height = "300px", solidHeader = TRUE, status = "primary",
-                    radioButtons("filter", label = "Filter Par :", choices = filtre_list))
+                box(width = 2, height = "320px",collapsible = TRUE, background = "navy",
+                    radioButtons("filtre", label = "Filtrer Par:", choices = choix, selected = "Classification"),
+                    br(),
+                    selectInput("choix_ville", label = "Choisir Municipalité", choices = list_ville, selected = "Tous")
+                    ),
+                box(width = 5,height = "320px",plotOutput("bar_chart",height = "300px"), status = "primary"),
+                box(width = 5,height = "320px",plotOutput("bar_chart_type",height = "300px"), status = "primary")
               ),
+              
+              #----------------- Ligne 3: Le tableau de établissements et la map ---------------
               fluidRow(
-                box(width = 8,height = "300px" ,leafletOutput("map",height = "280px"), status = "primary"),
-                box(title = "Filtre", width = 4, height = "300px", solidHeader = TRUE, status = "primary",
-                    selectInput("filter", label = "Filter By  :", choices = filtre_list, multiple = FALSE))
+                box(title = textOutput("titre_tab"),DTOutput("list_hbgr", height = "340px"),width = 6, height = "400px",status = "primary"),
+                box(title = textOutput("titre_map"),width = 6,height = "400px", background = "navy" ,leafletOutput("map",height = "340px"), status = "primary")
               )
       ),
+      
+      #----------------------------------- Section 2: Data -------------------------------------
       tabItem(tabName = "data_table",
+              
+              #-------------------------------- Ligne 1: Filtre --------------------------------
               fluidRow(
-                box(title = "Choose dataset to visualize",
-                    selectInput("data_viz_in", "Choose Table", choices = c("Data 1", "Data 2", "Data 3")))
+                box(title = "Voir dataset:",background = "navy",
+                    selectInput("data_viz_in", "Choose Table", choices = c("JOINTURE ETBL & ADRESSES", "JOINTURE TYPE & CARACTÉRISTIQUES")))
               ),
+              
+              #-------------------------- Ligne 2: Affichage du tableau ------------------------
               fluidRow(
-                box(title = "Dataset", width = 12, dataTableOutput("data_viz_out", width = "100%", height = "auto"))
+                box(title = "Dataset", width = 12, DTOutput("data_viz_out"))
               )
-      )
+      ),
+      
+      #------------------------------------ Section 3: Team ------------------------------------
+      tabItem(tabName = "team",
+              
+              #---------------------------- Ligne 1: 2 profils ---------------------------------
+              fluidRow(
+                
+                #------------------------------------ Profil 1 ---------------------------------
+                box(
+                  title = "Profil",
+                  status = "primary",
+                  boxProfile(
+                    src = "",
+                    title = "Boukari Yameogo",
+                    subtitle = "MSc Intelligence d'affaires",
+                    boxProfileItemList(
+                      bordered = TRUE,
+                      boxProfileItem(
+                        title = "Prénom",
+                        description = "Boukari"
+                      ),
+                      boxProfileItem(
+                        title = "Nom",
+                        description = "Yameogo"
+                      ),
+                      boxProfileItem(
+                        title = "Matricule",
+                        description = 11264583
+                      ),
+                      boxProfileItem(
+                        title = "Programme",
+                        description = "Master Intelligence d'affaires"
+                      ),
+                      boxProfileItem(
+                        title = "Université",
+                        description = "HEC Montréal"
+                      )
+                    )
+                  )
+                ),
+                
+                #--------------------------------- Profil 2 ------------------------------------
+                box(
+                  title = "Profil",
+                  status = "primary",
+                  boxProfile(
+                    src = "",
+                    title = "Nicolas Girard",
+                    subtitle = "MSc Intelligence d'affaires",
+                    boxProfileItemList(
+                      bordered = TRUE,
+                      boxProfileItem(
+                        title = "Prénom",
+                        description = "Nicolas"
+                      ),
+                      boxProfileItem(
+                        title = "Nom",
+                        description = "Girard"
+                      ),
+                      boxProfileItem(
+                        title = "Matricule",
+                        description = "########"
+                      ),
+                      boxProfileItem(
+                        title = "Programme",
+                        description = "Master Intelligence d'affaires"
+                      ),
+                      boxProfileItem(
+                        title = "Université",
+                        description = "HEC Montréal"
+                      )
+                    )
+                  )
+                )
+              ),
+              
+              #--------------------------------- Ligne 2: 2 profils -----------------------------
+              fluidRow(
+                
+                #--------------------------------- Profil 3 ------------------------------------
+                box(
+                  title = "Profil",
+                  status = "primary",
+                  boxProfile(
+                    src = "",
+                    title = "Eliette Compaore",
+                    subtitle = "MSc Intelligence d'affaires",
+                    boxProfileItemList(
+                      bordered = TRUE,
+                      boxProfileItem(
+                        title = "Prénom",
+                        description = "Eliette"
+                      ),
+                      boxProfileItem(
+                        title = "Nom",
+                        description = "Compaore"
+                      ),
+                      boxProfileItem(
+                        title = "Matricule",
+                        description = "########"
+                      ),
+                      boxProfileItem(
+                        title = "Programme",
+                        description = "Master Intelligence d'affaires"
+                      ),
+                      boxProfileItem(
+                        title = "Université",
+                        description = "HEC Montréal"
+                      )
+                    )
+                  )
+                ),
+                
+                #--------------------------------- Profil 4 ------------------------------------
+                box(
+                  title = "Profil",
+                  status = "primary",
+                  boxProfile(
+                    src = "",
+                    title = "Imene Abid",
+                    subtitle = "Baccalauréat",
+                    boxProfileItemList(
+                      bordered = TRUE,
+                      boxProfileItem(
+                        title = "Prénom",
+                        description = "Imene"
+                      ),
+                      boxProfileItem(
+                        title = "Nom",
+                        description = "Abid"
+                      ),
+                      boxProfileItem(
+                        title = "Matricule",
+                        description = "########"
+                      ),
+                      boxProfileItem(
+                        title = "Programme",
+                        description = "BAA"
+                      ),
+                      boxProfileItem(
+                        title = "Université",
+                        description = "HEC Montréal"
+                      )
+                    )
+                  )
+                )
+              )
+              )
     )
   )
 )
 
+
+#--------------- Concatenation des differents membres pour former l'application -----------
 ui <- dashboardPage(header, sidebar, body)
 
+#----------------------- Affichage du produit fini de l'interface -------------------------
 shinyUI(ui)
+
+#-------------------------------------- THE END -------------------------------------------
